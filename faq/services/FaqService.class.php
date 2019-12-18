6ce0c6f371f942a948f8930bffc475012595a4ff
mipel85 <mipel@phpboost.com>
2019-12-18 15:19:32 +0100

    [UPD] repo

--- faq/services/FaqService.class.php
+++ faq/services/FaqService.class.php
@@ -3,9 +3,8 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2019 12 18
+ * @version     PHPBoost 5.3 - last update: 2019 12 xx
  * @since       PHPBoost 4.0 - 2014 09 02
- * @contributor Mipel <mipel@phpboost.com>
  * @contributor Mipel <mipel@phpboost.com>
 */
 
@@ -83,13 +82,6 @@ class FaqService
 	}
     
 	public static function clear_cache()
-	{
-		Feed::clear_cache('faq');
-		FaqCache::invalidate();
-		FaqCategoriesCache::invalidate();
-	}
-	
-	public static function bidon()
 	{
 		Feed::clear_cache('faq');
 		FaqCache::invalidate();
