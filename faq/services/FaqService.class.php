4680627342df0c769e710f9caa38dc8befd55931
mipel85 <mipel@phpboost.com>
2019-12-18 15:16:37 +0100

    [UPD] add 2

--- faq/services/FaqService.class.php
+++ faq/services/FaqService.class.php
@@ -85,6 +85,8 @@ class FaqService
 	public static function clear_cache()
 	{
 		Feed::clear_cache('faq');
+		Feed::clear_cache('faq');
+		FaqCache::invalidate();
 		FaqCache::invalidate();
 		FaqCategoriesCache::invalidate();
 	}
