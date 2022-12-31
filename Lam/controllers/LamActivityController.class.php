4326369c387af185d9096d9fe4dd86cbd7587bbc
mipel85 <mipel85@phpboost.com>
2022-12-31 17:14:21 +0100

    [ADD] send mail

--- Lam/controllers/LamActivityController.class.php
+++ Lam/controllers/LamActivityController.class.php
@@ -8,14 +8,16 @@
  */
 class LamActivityController extends DefaultModuleController
 {
+    private $email_form;
 
     public function execute(HTTPRequestCustom $request)
     {
-//        $this->check_authorizations();
+        $this->check_authorizations();
         $this->build_form($request);
 
         if ($this->submit_button->has_been_submited() && $this->form->validate()){
             $this->save();
+            $this->send_form_email();
             // $this->redirect();
         }
 
@@ -36,31 +38,30 @@ class LamActivityController extends DefaultModuleController
         $choices = new FormFieldsetHTML('form_name', $this->lang['lam.form.choices']);
         $form->add_fieldset($choices);
 
-        $choices->add_field(new FormFieldRadioChoice('form_radio', $this->lang['lam.form.activity.type'], '', 
-          array(
-            new FormFieldRadioChoiceOption($this->lang['lam.jpo'], 'jpo'),
-            new FormFieldRadioChoiceOption($this->lang['lam.qpdd'], 'qpdd')
-                )
+        $choices->add_field(new FormFieldRadioChoice('form_radio', $this->lang['lam.form.activity.type'], '', array(
+            new FormFieldRadioChoiceOption($this->lang['lam.jpo'], $this->lang['lam.jpo']),
+            new FormFieldRadioChoiceOption($this->lang['lam.qpdd'], $this->lang['lam.qpdd'])
+                ), array('required' => true, 'class' => 'css-class')
         ));
 
-
-        $fieldset->add_field(new FormFieldTextEditor('club_name', $this->lang['lam.club.name'], ''));
-        $fieldset->add_field(new FormFieldNumberEditor('club_ffam_number', $this->lang['lam.club.ffam.number'], ''));
-        $fieldset->add_field(new FormFieldDate('club_activity_date', $this->lang['lam.club.activity.date'], null));
+        // other fields
+        $fieldset->add_field(new FormFieldTextEditor('club_name', $this->lang['lam.club.name'], '', array('required' => true, 'class' => 'css-class')));
+        $fieldset->add_field(new FormFieldNumberEditor('club_ffam_number', $this->lang['lam.club.ffam.number'], '', array('required' => true, 'class' => 'css-class')));
+        $fieldset->add_field(new FormFieldDate('club_activity_date', $this->lang['lam.club.activity.date'], null, array('required' => true, 'class' => 'css-class')));
         $fieldset->add_field(new FormFieldTextEditor('club_activity_location', $this->lang['lam.club.activity.location'], ''));
         $fieldset->add_field(new FormFieldTextEditor('club_activity_city', $this->lang['lam.club.activity.city'], ''));
         $fieldset->add_field(new FormFieldTextEditor('club_activity_description', $this->lang['lam.club.activity.description'], ''));
         $fieldset->add_field(new FormFieldTextEditor('club_sender_name', $this->lang['lam.club.sender.name'], ''));
-        $fieldset->add_field(new FormFieldTextEditor('club_sender_mail', $this->lang['lam.club.sender.mail'], ''));
+        $fieldset->add_field(new FormFieldMailEditor('club_sender_mail', $this->lang['lam.club.sender.mail'], ''));
 
         $form->add_fieldset($fieldset);
-// End form
 
-        $this->submit_button = new FormButtonDefaultSubmit();
+        $this->submit_button = new FormButtonDefaultSubmit('Envoyer la demande', '', '');
         $form->add_button($this->submit_button);
-        $form->add_button(new FormButtonReset());
+        $form->add_button(new FormButtonReset('Annuler'));
 
         $this->form = $form;
+// End form
     }
 
     private function get_item()
@@ -84,13 +85,13 @@ class LamActivityController extends DefaultModuleController
 
     private function check_authorizations()
     {
-        // if (!$this->config->is_authorized_to_send())
-        // {
-        // 	$error_controller = PHPBoostErrors::user_not_authorized();
-        // 	DispatchManager::redirect($error_controller);
-        // }
-        if (AppContext::get_current_user()->is_readonly()){
-            $controller = PHPBoostErrors::user_in_read_only();
+//         if (!$this->config->is_authorized_to_send()){
+//            $error_controller = PHPBoostErrors::user_not_authorized();
+//            DispatchManager::redirect($error_controller);
+//        }
+
+        if (!AppContext::get_current_user()->check_level(User::MEMBER_LEVEL)){
+            $controller = PHPBoostErrors::user_not_authorized();
             DispatchManager::redirect($controller);
         }
     }
@@ -99,6 +100,7 @@ class LamActivityController extends DefaultModuleController
     {
 
         $item = $this->get_item();
+        $item->set_form_date(new Date());
         $item->set_form_name($this->form->get_value('form_radio')->get_raw_value());
         $item->set_club_name($this->form->get_value('club_name'));
         $item->set_club_ffam_number($this->form->get_value('club_ffam_number'));
@@ -106,8 +108,7 @@ class LamActivityController extends DefaultModuleController
         $item->set_club_activity_location($this->form->get_value('club_activity_location'));
         $item->set_club_activity_city($this->form->get_value('club_activity_city'));
         $item->set_club_activity_description($this->form->get_value('club_activity_description'));
-        $item->set_club_sender_name($this->form->get_value('club_sender_name'));
-        $item->set_club_sender_mail($this->form->get_value('club_sender_mail'));
+
         $id = LamService::add($item);
         $item->set_id($id);
     }
@@ -123,6 +124,67 @@ class LamActivityController extends DefaultModuleController
         $breadcrumb->add($this->lang['lam.form'], LamUrlBuilder::activity());
 
         return $response;
+    }
+ /*   
+    private function send_item_email()
+	{
+		$item_message = '';
+		$item_subject = $this->item->get_title();
+		$item_sender_name = $this->email_form->get_value('sender_name');
+		$item_sender_email = $this->email_form->get_value('sender_email');
+		$item_message = $this->email_form->get_value('sender_message');
+		$item_recipient_email = $this->item->get_custom_author_email();
+
+		$item_email = new Mail();
+		$item_email->set_sender(MailServiceConfig::load()->get_default_mail_sender(), $this->lang['smallads.module.title']);
+		$item_email->set_reply_to($item_sender_email, $item_sender_name);
+		$item_email->set_subject($item_subject);
+		$item_email->set_content(TextHelper::html_entity_decode($item_message));
+		$item_email->add_recipient($item_recipient_email);
+
+		$send_email = AppContext::get_mail_service();
+
+		return $send_email->try_to_send($item_email);
+	}
+*/
+    private function send_form_email()
+    {
+       // $activity_date = Date::to_format($this->form->get_value('club_activity_date')->get_timestamp(), Date::FORMAT_DAY_MONTH_YEAR);
+                
+        $item_message = '';
+        $item_subject = $this->lang['lam.activity.desc'] . ' : ' . $this->form->get_value('form_radio')->get_raw_value();
+        $item_sender_name = $this->form->get_value('club_sender_name');
+        $item_sender_email = $this->form->get_value('club_sender_email');
+        
+        //msg content
+        $item_message = StringVars::replace_vars($this->lang['lam.mail.msg'], array(
+        'club_sender_name' => $this->form->get_value('club_sender_name'),
+        'club_sender_mail' => $this->form->get_value('club_sender_mail'),
+        'club_name' => $this->form->get_value('club_name'), 
+        'club_ffam_number' => $this->form->get_value('club_ffam_number'), 
+        'activity' => $this->form->get_value('form_radio')->get_raw_value(),
+        'club_activity_date' => Date::to_format($this->form->get_value('club_activity_date')->get_timestamp(), Date::FORMAT_DAY_MONTH_YEAR),
+        'club_activity_location' => $this->form->get_value('club_activity_location'),
+        'club_activity_city' => $this->form->get_value('club_activity_city'),
+        
+        ));
+         
+     
+       
+//        $item_recipient_email = $this->item->get_custom_author_email();
+        $item_recipient_email = 'mipel85@gmail.com';
+
+        $item_email = new Mail();
+        $item_email->set_sender(MailServiceConfig::load()->get_default_mail_sender(), $this->lang['lam.form']);
+        $item_email->set_reply_to($item_sender_email, $item_sender_name);
+        $item_email->set_subject($item_subject);
+        $item_email->set_content(TextHelper::html_entity_decode($item_message));
+       
+        $item_email->add_recipient($item_recipient_email);
+ //Debug::stop($item_email);
+        $send_email = AppContext::get_mail_service();
+
+        return $send_email->try_to_send($item_email);
     }
 }
 ?>
