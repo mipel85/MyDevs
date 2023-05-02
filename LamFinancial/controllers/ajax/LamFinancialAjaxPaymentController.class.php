fe4181e72f903cd2915d922551aa1da488019c5a
mipel85 <mipel85@phpboost.com>
2023-05-02 09:56:19 +0200

    [DEL] readme [UPD] date

--- LamFinancial/controllers/ajax/LamFinancialAjaxPaymentController.class.php
+++ LamFinancial/controllers/ajax/LamFinancialAjaxPaymentController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Mipel85 <mipel@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2023 05 01
+ * @version     PHPBoost 6.0 - last update: 2023 05 02
  * @since       PHPBoost 6.0 - 2022 12 20
  */
 class LamFinancialAjaxPaymentController extends DefaultModuleController
--- README.md
+++
@@ -1,2 +0,0 @@
-# MyDevs
-Module allowing to scan the files used in all the contents and to make an analysis of them.
