40b8df0b9a801084f6bdb5cfbd1b2ae02f2cf50c
babsolune <babsolune@phpboost.com>
2023-12-08 08:27:49 +0100

    [UPD] 404 & landing

--- petanque/index.php
+++ petanque/index.php
@@ -1,44 +1,54 @@
 <?php
 
-    require_once('./classes/install.class.php');
-    // automatic installation of database and tables
-    install::create_database();
-    install::create_players_table();
-    install::insert_data_players();
+require_once('./classes/install.class.php');
 
-    $page = $_GET['page'] ?? '404';
+// automatic installation of database and tables
+install::create_database();
+install::create_players_table();
+install::insert_data_players();
 
-    $title = '';
-    switch($page)
-    {
-        case ('config') : $title = 'Administration'; break;
-        case ('selection') : $title = 'Sélection'; break;
-        case ('party') : $title = 'Partie'; break;
-        case ('ranking') : $title = 'Classement'; break;
-        default : $title = 'Accueil'; break;
-    }
+$page = $_GET['page'] ?? '404';
 
-    require_once('./theme/templates/header.controller.php');
+$url = $_SERVER['REQUEST_URI'];
+$url = explode('/', $url);
+$url = end($url);
+if ($url == '')
+    $page = 'home';
 
-    switch($page)
-    {
-        case ('config') :
-            require_once('./theme/templates/config.controller.php');
-            break;
-        case ('selection') :
-            require_once('./theme/templates/selection.controller.php');
-            break;
-        case ('party') :
-            require_once('./theme/templates/party.controller.php');
-            break;
-        case ('ranking') :
-            require_once('./theme/templates/ranking.controller.php');
-            break;
-        default :
-            require_once('./theme/templates/home.controller.php');
-            break;
-    }
+$title = '';
+switch($page)
+{
+    case ('config') : $title = 'Administration'; break;
+    case ('selection') : $title = 'Sélection'; break;
+    case ('party') : $title = 'Partie'; break;
+    case ('ranking') : $title = 'Classement'; break;
+    default : $title = 'Accueil'; break;
+}
 
-    require_once('./theme/templates/footer.controller.php');
+require_once('./theme/templates/header.controller.php');
+
+switch($page)
+{
+    case ('config') :
+        require_once('./theme/templates/config/config.controller.php');
+        break;
+    case ('home') :
+        require_once('./theme/templates/home.controller.php');
+        break;
+    case ('selection') :
+        require_once('./theme/templates/selection.controller.php');
+        break;
+    case ('party') :
+        require_once('./theme/templates/party.controller.php');
+        break;
+    case ('ranking') :
+        require_once('./theme/templates/ranking.controller.php');
+        break;
+    default :
+        require_once('./theme/templates/404.controller.php');
+        break;
+}
+
+require_once('./theme/templates/footer.controller.php');
 ?>
 
--- petanque/theme/templates/404.controller.php
+++ petanque/theme/templates/404.controller.php
@@ -1,1 +1,6 @@
-404 error
+<section></section>
+    <header class="section-header">
+        <h1>Erreur 404</h1>
+    </header>
+    <article class="content">La page n'a pu être trouvée</article>
+</section>
