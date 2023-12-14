d57ac7c13ca45a530339836ad4cc87fc7198a703
babsolune <babsolune@phpboost.com>
2023-12-14 09:54:06 +0100

    [FIX] caps off

--- petanque/theme/templates/404.view.php
+++
@@ -1,6 +0,0 @@
-<section></section>
-    <header class="section-header">
-        <h1>Erreur 404</h1>
-    </header>
-    <article class="content">La page n'a pu être trouvée</article>
-</section>
--- petanque/theme/templates/ranking.view.php
+++
@@ -1,7 +0,0 @@
-<section>
-    <header class="section-header"><h1>Classement</h1></header>
-    <article class="content">
-        <span class="description">Tableau du classement des joueurs</span>
-    </article>
-    <footer></footer>
-</section>
