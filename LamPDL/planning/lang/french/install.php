133e25ebca5831ced86bbafb1cce08f7a2336c86
mipel85 <mipel85@phpboost.com>
2024-10-19 10:50:13 +0200

    [ADD] planning add cats

--- LamPDL/planning/lang/french/install.php
+++ LamPDL/planning/lang/french/install.php
@@ -3,17 +3,29 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2024 01 20
- * @since       PHPBoost 6.0 - 2020 01 18
-*/
+ * @version     PHPBoost 6.0 - last update: 2024 10 19
+ * @since       PHPBoost 6.0 - 2024 02 10
+ */
 
 ####################################################
 #                    French                        #
 ####################################################
 
-$lang['planning.category.1'] = 'Portes-Ouvertes';
-$lang['planning.category.2'] = 'Bourse d\'échange';
-$lang['planning.category.3'] = 'Interclubs Indoor';
-$lang['planning.category.4'] = 'Rencontres Extérieur';
-$lang['planning.category.5'] = 'GPR';
+$lang['planning.category.1'] = 'Compétition Départementale';
+$lang['planning.category.2'] = 'Compétition Fédérale';
+$lang['planning.category.3'] = 'Compétition Régionale';
+$lang['planning.category.4'] = 'Exposition, brocante, bourse d\'échange';
+$lang['planning.category.5'] = 'Journée annuelle des associations, Téléthon';
+$lang['planning.category.6'] = 'Journée d\'initiation';
+$lang['planning.category.7'] = 'Journée portes-ouvertes (extérieur ou indoor)';
+$lang['planning.category.8'] = 'Manifestation privée extérieure';
+$lang['planning.category.9'] = 'Manifestation privée intérieure';
+$lang['planning.category.10'] = 'Séance de passage ailes, rotors ou brevets';
+$lang['planning.category.11'] = 'Séance de passage QPDD';
+$lang['planning.category.12'] = 'Spectacle aérien public extérieur (SAPA extérieur)';
+$lang['planning.category.13'] = 'Spectacle aérien public intérieur (SAPA intérieur)';
+$lang['planning.category.14'] = 'Stage examinateur QPDD';
+$lang['planning.category.15'] = 'Stage d\'initiation ou de formation';
+$lang['planning.category.16'] = 'Stage de juges';
+$lang['planning.category.17'] = 'Stage préparation passage QPDD';
 ?>
