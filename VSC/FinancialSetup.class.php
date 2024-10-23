73892026d3dc4833efda90b7a1fa3d128ce0788c
Mipel <mipel@phpboost.com>
2024-10-23 12:30:16 +0200

    date

--- VSC/FinancialSetup.class.php
+++ VSC/FinancialSetup.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2024 10 17
+ * @version     PHPBoost 6.0 - last update: 2024 10 23
  * @since       PHPBoost 6.0 - 2020 01 18
  */
 
