d7d2b57a5c00cabfeb27cf104ba80ccd101fbd19
Mipel <mipel@phpboost.com>
2024-10-23 13:39:06 +0200

    vsc clean

--- VSC/index.php
+++ VSC/index.php
@@ -1,3 +1,12 @@
 <?php
+
 phpinfo();
-?>
+function func_get_arg()
+{
+    $raw = 'toto';
+    $tab  = array(
+        'mimi' => 'jean',
+        'mimi' => 'jean',
+        'mimi' => 'jean',
+    );
+}
