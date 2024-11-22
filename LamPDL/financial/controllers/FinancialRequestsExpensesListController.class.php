15e292436f7d869fdf127843b6873190de63ba8c
Mipel <mipel@phpboost.com>
2024-11-22 18:15:49 +0100

    sup espace l112

--- LamPDL/financial/controllers/FinancialRequestsExpensesListController.class.php
+++ LamPDL/financial/controllers/FinancialRequestsExpensesListController.class.php
@@ -4,7 +4,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2024 11 15
+ * @version     PHPBoost 6.0 - last update: 2024 11 22
  * @since       PHPBoost 6.0 - 2024 02 09
  * @contributor Mipel <mipel@phpboost.com>
  */
