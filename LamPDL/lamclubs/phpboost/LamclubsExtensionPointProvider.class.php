db045b7a570ce559cea7af7c44d195d3c0c69887
mipel85 <mipel85@phpboost.com>
2024-12-03 11:50:26 +0100

    [UPD] remove the module from the search scope

--- LamPDL/lamclubs/phpboost/LamclubsExtensionPointProvider.class.php
+++ LamPDL/lamclubs/phpboost/LamclubsExtensionPointProvider.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2024 01 20
+ * @version     PHPBoost 6.0 - last update: 2024 12 03
  * @since       PHPBoost 6.0 - 2024 01 18
 */
 
@@ -28,5 +28,10 @@ class LamclubsExtensionPointProvider extends ItemsModuleExtensionPointProvider
 	{
 		return false;
 	}
+
+    public function search()
+    {
+        return false;
+    }
 }
 ?>
