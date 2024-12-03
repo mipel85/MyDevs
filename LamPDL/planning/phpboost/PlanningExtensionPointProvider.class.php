b61b38fd276bd775dec2cae9f3a3cb61160a0689
mipel85 <mipel85@phpboost.com>
2024-12-03 11:59:51 +0100

    [UPD]add search parameters

--- LamPDL/planning/phpboost/PlanningExtensionPointProvider.class.php
+++ LamPDL/planning/phpboost/PlanningExtensionPointProvider.class.php
@@ -3,15 +3,15 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2024 01 20
+ * @version     PHPBoost 6.0 - last update: 2024 12 03
  * @since       PHPBoost 6.0 - 2020 01 18
-*/
+ */
 
 class PlanningExtensionPointProvider extends ItemsModuleExtensionPointProvider
 {
-	public function home_page()
-	{
-		return new DefaultHomePageDisplay($this->get_id(), PlanningHomeController::get_view());
-	}
+    public function home_page()
+    {
+        return new DefaultHomePageDisplay($this->get_id(), PlanningHomeController::get_view());
+    }
 }
 ?>
--- LamPDL/planning/phpboost/PlanningSearchable.class.php
+++ LamPDL/planning/phpboost/PlanningSearchable.class.php
@@ -3,23 +3,22 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2024 01 20
+ * @version     PHPBoost 6.0 - last update: 2024 12 03
  * @since       PHPBoost 6.0 - 2020 01 18
-*/
+ */
 
 class PlanningSearchable extends DefaultSearchable
 {
-	public function __construct()
-	{
-		parent::__construct('planning');
 
-		$this->table_name = PlanningSetup::$planning_table;
+    public function __construct()
+    {
+        $module_id = 'planning';
+        parent::__construct($module_id);
 
-		$this->field_id = 'id';
-		$this->field_rewrited_link = 'rewrited_link';
-		$this->field_content = 'content';
-
-		$this->field_published = 'approved';
-	}
+        $this->table_name           = PlanningSetup::$planning_table;
+        $this->field_title          = 'rewrited_link';
+        $this->field_rewrited_title = 'rewrited_link';
+        $this->field_published      = 'approved';
+    }
 }
 ?>
