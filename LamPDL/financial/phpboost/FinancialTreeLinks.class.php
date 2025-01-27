01f30877d4c29cf1250f79a6db5d5c1be0fb85bd
mipel85 <mipel85@phpboost.com>
2025-01-27 13:30:43 +0100

    [UPD] select date to current year

--- LamPDL/financial/phpboost/FinancialTreeLinks.class.php
+++ LamPDL/financial/phpboost/FinancialTreeLinks.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2024 12 18
+ * @version     PHPBoost 6.0 - last update: 2025 01 27
  * @since       PHPBoost 6.0 - 2020 01 18
 */
 
@@ -22,7 +22,7 @@ class FinancialTreeLinks implements ModuleTreeLinksExtensionPoint
         
         $financial_statement = new AdminModuleLink($lang['financial.statement'], '');
         $financial_statement->add_sub_link(new AdminModuleLink($lang['financial.chart.budgets.used'], FinancialUrlBuilder::requests_monitoring_chart()));
-        $financial_statement->add_sub_link(new AdminModuleLink($lang['financial.budgets.statement'], FinancialUrlBuilder::requests_monitoring_list($year)));
+        $financial_statement->add_sub_link(new AdminModuleLink($lang['financial.budgets.statement'], FinancialUrlBuilder::requests_monitoring_list('')));
 		$tree->add_link($financial_statement);
         
         if (!empty($budget_archive_tables)) {
